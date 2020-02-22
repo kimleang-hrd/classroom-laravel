c2ac3c366147518fb9edb983985f0df48871b2a3
Kimleang <kimleang.hrd@gmail.com>
2020-02-22 10:39:34 +0700

    Design Classroom Cart blade

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,12 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique()->nullable();
+            $table->string('email')->unique()->nullable(true);
             $table->string('password');
-            $table->unsignedInteger('role_id')->nullable(false);
-            $table->foreign('role_id')
-                ->references('id')
-                ->on('roles');
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,29 @@
 
 @section('content')
 <div class="container">
+    <div class="row">
+        <div class="col-3">
+            <div class="card">
+                <img class="card-img-top" src="https://techteachgoals.files.wordpress.com/2017/08/classroom.jpg" alt="Card image cap">
+                <div class="card-body"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Microsoft_Account.svg/1200px-Microsoft_Account.svg.png" alt="Logo"
+                                            style="border-radius: 50%;
+    float: right;
+    height: 4.6875rem;
+    position: relative;
+    margin-top: -5.8rem;
+    width: 4.6875rem;
+}">
+                    <h5 class="card-title">Laravel Class</h5>
+                    <p class="card-text">SUbject : haha</p>
+                    <p class="card-text">Student: 80 nak</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">created 3 days ago</small>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -14,7 +37,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged in
                 </div>
             </div>
         </div>
